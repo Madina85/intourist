8fe0813711e19749df064c6e6535955648fd76fb
madina myrzakulova <madina.japarbekovna@jmail.com>
2022-03-18 16:19:27 +0600

    add unit test

---
+++ places/factories.py
@@ -0,0 +1,12 @@
+import factory
+from places.models import Place
+
+
+
+class PlaceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Place
+
+    name = factory.Sequence(lambda n: f"Place number {n}")
+    location = factory.Sequence(lambda n: f"location - {n}")
+    description =  factory.Sequence(lambda n: f"description-{n}")   
